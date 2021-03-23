5642cbd31d856e434e39d9ddd63a7f15dbaa524d
BenPelczynski <bepelczynski@davidson.edu>
2021-03-23 00:20:07 -0400

    Added rotation

--- Assets/Scripts/ShipManeuver.cs
+++ Assets/Scripts/ShipManeuver.cs
@@ -2,49 +2,94 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShipManeuver : MonoBehaviour
-{
-   
-   private Rigidbody2D rb;
-   
-   float maxVelocity = 3;
-   
-    // Start is called before the first frame update
-    private void Start()
+
+// this is the 2D version
+
+public class ShipManeuver : PhysicsBase {
+
+    public Vector3 direction;           // the direction vector
+
+
+    // for rotation
+    public float rotateTimeElapsed = 0;
+    public float rotateDuration = 200;
+
+    public float inputRotateTimeElapsed = 0;
+    public float inputRotateDuration = 200;
+
+
+    public bool receivingInput;
+
+    private void Start ()
     {
-       rb = GetComponent<Rigidbody2D>();
+        // get container collider
     }
-    
-    // Update is called once per frame
-    private void Update()
+
+    protected override void Update ()
     {
-        float yAxis = Input.GetAxis("Vertical");
-        float xAxis = Input.GetAxis("Horizontal");
-        ThrustSideways(xAxis); 
-        ThrustForward(yAxis);       
-     }
-    
-    #region Manuevering API
-    
-    private void ClampVelocity()
-    {
-    		float x = Mathf.Clamp(rb.velocity.x, -maxVelocity, maxVelocity);
-    		float y = Mathf.Clamp(rb.velocity.y, -maxVelocity, maxVelocity);
-    		
-    		rb.velocity = new Vector2(x, y);
+        base.Update ();
+
     }
-    
-    private void ThrustForward(float amount)
+
+    void FixedUpdate ()
     {
-    		Vector2 force = transform.up * amount;
-    		rb.AddForce(force);
+       
+        // if player input is happening and the current object is targeted
+        if (playerInput.magnitude != 0) {
+            // get player input
+            direction = playerInput;
+            // is someone pressing buttons
+            receivingInput = true;
+        } else {
+            // get direction
+            receivingInput = false;
+        }
+
+        // distance to move each frame = normalized distance vector * speed * time since last frame
+        Vector3 step = direction * thrust * Time.deltaTime;
+
+        // add step vector to current position
+        rb.MovePosition (transform.position + step);
+        //rb.velocity = direction * thrust;
+
+        // is someone pressing buttons
+        if (receivingInput == true) {
+            // rotate to that direction
+            RotateTorwardsDirection2D ();
+
+        } 
+
     }
-    
-     private void ThrustSideways(float amount)
+
+
+    /**
+     *  Turn transform towards a direction vector over time
+     */
+    void RotateTorwardsDirection2D ()
     {
-    		Vector2 force = transform.right * amount;
-    		rb.AddForce(force);
+        // Reset other rotate time elapsed if not zero
+        if (rotateTimeElapsed != 0) rotateTimeElapsed = 0;
+
+        // Set position to rotate towards based on player input
+        Vector3 rotateTo = transform.position + (playerInput * 100);
+
+        // change look direction slowly
+        Vector3 temp = Vector3.Lerp (transform.right, (rotateTo - transform.position), inputRotateTimeElapsed / inputRotateDuration);
+
+        // Prevent possible flipping in y rotation
+        Transform cloneTransform = transform;
+        cloneTransform.right = temp;
+        if (cloneTransform.eulerAngles.y != 0) {
+            temp = new Vector3 (temp.x, 0.1f, 0);
+        }
+
+        // Set right vector
+        transform.right = temp;
+
+        inputRotateTimeElapsed += Time.deltaTime;
     }
-    
-    #endregion
+
+
+
+
 }
