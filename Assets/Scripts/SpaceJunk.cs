a5b18d3a6c054bc7385e709d3bbf429d3e8f1a07
Mary Grace Bunch <mabunch@davidson.edu>
2021-04-26 15:14:04 -0400

    space junk moving across screen in separate scene

--- Assets/Scripts/SpaceJunk.cs
+++ Assets/Scripts/SpaceJunk.cs
@@ -2,36 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// where to attach this script? 
+// deals with each instance of space junk
 public class SpaceJunk : MonoBehaviour
 {
-    public Vector3 direction;
-
-    public BoxCollider perimeterContainerCollider; // assign this to JunkBoundary
-    public Vector3 wayPoint; // new point to head towards
+    public Vector3 origin;
+    public Vector3 destination;
+    public float speed = 5f;
 
     // Start is called before the first frame update
-    void Start()
+    public void Init(Vector3 start, Vector3 target)
     {
-        
+        origin = start;
+        destination = target;
     }
 
     // Update is called once per frame
-    void FixedUpdate()
-    {
-        // find a random location within box colliders.
-        Vector2 startPoint = FindPointInBounds(perimeterContainerCollider.bounds);
-
-        // place game object (space junk) in that location (transform position?)
-
-        // move game object towards wayPoint (which is a Vector3)
-        
+    void Update()    {
+        // move game object from origin to destination
+        // distance to move each frame = normalized distance vector * speed * time since last frame
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, destination, step);
     }
 
-    public Vector2 FindPointInBounds(Bounds bounds)
-    {
-        return new Vector2(
-            Random.Range(bounds.min.x, bounds.max.x),
-            Random.Range(bounds.min.y, bounds.max.y));
-    }
 }
