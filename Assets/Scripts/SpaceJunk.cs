0a2fbfce5d3ce690bdfca9a67cfc5cb2df07f2d3
Mary Grace Bunch <mabunch@davidson.edu>
2021-04-26 23:56:33 -0400

    improved scene with more randomization

--- Assets/Scripts/SpaceJunk.cs
+++ Assets/Scripts/SpaceJunk.cs
@@ -7,13 +7,14 @@ public class SpaceJunk : MonoBehaviour
 {
     public Vector3 origin;
     public Vector3 destination;
-    public float speed = 5f;
+    public float speed;
 
     // Start is called before the first frame update
     public void Init(Vector3 start, Vector3 target)
     {
         origin = start;
         destination = target;
+        speed = Random.Range(5,15); // randomize speed of junks movement
     }
 
     // Update is called once per frame
@@ -21,7 +22,14 @@ public class SpaceJunk : MonoBehaviour
         // move game object from origin to destination
         // distance to move each frame = normalized distance vector * speed * time since last frame
         float step = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, destination, step);
+        if (transform.position == destination)
+        {
+            Destroy(gameObject); // get rid of game object once it has reached its destination
+
+        }else{
+            transform.position = Vector3.MoveTowards(transform.position, destination, step);
+        }
+        
     }
 
 }
