84486cec7f7371a70e43ca081eedf86d0415d787
Owen Corkery <owcorkery@davidson.edu>
2021-05-09 18:53:58 -0400

    fixed ball collision

--- Assets/Scripts/ShipExplosion.cs
+++ Assets/Scripts/ShipExplosion.cs
@@ -8,11 +8,11 @@ public class ShipExplosion : MonoBehaviour
     Animator animator;
 
     string animationState = "ShipState";
-    
+
     public GameObject mball;
 
     public GameObject shield;
-    
+
     public Score score;
 
     enum CharStates
@@ -33,17 +33,17 @@ public class ShipExplosion : MonoBehaviour
     {
     }
 
-    IEnumerator OnTriggerEnter2D(Collider2D theCollision)
+    IEnumerator OnCollisionEnter2D(Collision2D theCollision)
     {
       GameObject go = GameObject.Find ("PlayerBody");
       GameObject ball = GameObject.Find ("Circle");
-	
+
 	if (theCollision.gameObject.name == "PowerUp_Combo(Clone)"){
 	 	score.value = score.value + score.points + 500;
       	score.points = score.points + 500;
         	Destroy(theCollision.gameObject);
 	 }
-	
+
 	else if (theCollision.gameObject.name == "PowerUp_Multi(Clone)"){
 	 	Instantiate(mball, theCollision.gameObject.transform.position, Quaternion.identity);
         	Destroy(theCollision.gameObject);
@@ -53,10 +53,10 @@ public class ShipExplosion : MonoBehaviour
 	 	Instantiate(shield, theCollision.gameObject.transform.position, Quaternion.identity);
         	Destroy(theCollision.gameObject);
 	 }
-	
+
       else if (theCollision.gameObject.name != "Circle" && theCollision.gameObject.name != "Shield(Clone)" && theCollision.gameObject.name != "MultiBall(Clone)"
       && theCollision.gameObject.name != "Bomb"){
-        
+
         Debug.Log(theCollision.gameObject.name);
         go.GetComponent<ShipManeuver>().enabled = false;
         ball.GetComponent<Collider2D>().enabled = false;
